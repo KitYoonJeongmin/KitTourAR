4296978453c090bf193d618ba90fec9dc526848c
KitYoonJeongmin <102850131+KitYoonJeongmin@users.noreply.github.com>
2022-11-07 23:24:47 +0900

    compass를 위한 코드 추가, magneticHeading에 북극까지의 roatate 저장

--- Assets/Scripts/GPS.cs
+++ Assets/Scripts/GPS.cs
@@ -14,6 +14,10 @@ public class GPS : MonoBehaviour
     public float maxWaitTime = 10.0f;
     public float resendTime = 1.0f;
 
+    //나침반 각도 (y축 rotate에 사용)
+    public static float magneticHeading;
+    public static float trueHeading;
+
     //위도 경도 변경
     public float latitude = 0;
     public float longitude = 0;
@@ -32,7 +36,7 @@ public class GPS : MonoBehaviour
     }
     void Start()
     {
-        StartCoroutine(GPS_On());   
+        StartCoroutine(GPS_On());
     }
 
     //GPS처리 함수
@@ -58,6 +62,8 @@ public class GPS : MonoBehaviour
 
         //위치 데이터를 요청 -> 수신 대기
         Input.location.Start(2.0f, 1.0f);
+        //나침반 활성화
+        Input.compass.enabled = true;
 
         //GPS 수신 상태가 초기 상태에서 일정 시간 동안 대기함
         while (Input.location.status == LocationServiceStatus.Initializing && waitTime < maxWaitTime)
@@ -81,13 +87,13 @@ public class GPS : MonoBehaviour
         }
 
         //수신된 GPS 데이터를 화면에 출력/
-       
+
         LocationInfo li = Input.location.lastData;
         latitude = li.latitude;
         longitude = li.longitude;
         latitude_text.text = "위도 : " + latitude.ToString();
         longitude_text.text = "경도 : " + longitude.ToString();
-        
+
         //위치 정보 수신 시작 체크
         receiveGPS = true;
 
@@ -97,7 +103,13 @@ public class GPS : MonoBehaviour
             li = Input.location.lastData;
             latitude = li.latitude;
             longitude = li.longitude;
-
+            //나침반 값 가져오기
+            if (Input.compass.headingAccuracy == 0 || Input.compass.headingAccuracy > 0)
+            {
+                magneticHeading = Input.compass.magneticHeading; // 자북극
+                trueHeading = Input.compass.trueHeading; // 지리적 북극
+            }
+            //ui에 출력
             latitude_text.text = "위도 : " + latitude.ToString();
             longitude_text.text = "경도 : " + longitude.ToString();
             unityCoor = GPSEncoder.GPSToUCS(latitude, longitude);
