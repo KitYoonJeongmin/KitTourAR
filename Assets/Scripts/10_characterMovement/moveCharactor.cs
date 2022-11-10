4f5e4028add27958cb6309b4244b0c2ef407b7a3
jangik <zmzf1@naver.com>
2022-11-11 07:48:11 +0900

    코드 정리

--- Assets/Scripts/10_characterMovement/moveCharactor.cs
+++ Assets/Scripts/10_characterMovement/moveCharactor.cs
@@ -6,44 +6,36 @@ public class moveCharactor : MonoBehaviour
 {
     public GameObject GPSManager;
     public Camera cam;
-    public Vector3 locationVec;
-    public Vector3 checkloc;
-    private float mov_speed = 10.0f;
-    private float rot_speed = 50.0f;
+    private Vector3 locationVec;
+    public float lat = 36.142952f;
+    public float lon = 128.394272f;
+    //private float mov_speed = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
         //GPSManager = GameObject.Find("GPSManager");
-        checkloc = GPSEncoder.GPSToUCS(36.142164f, 128.393760f);
         GetLatitude();
-        location();
-        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //KeybordMove();
+        charactorMove();
+    }
+
+    void charactorMove()
+    {
+        GetLatitude();
+        transform.position = locationVec;
     }
     void GetLatitude()
     {
         //locationVec = GPSManager.GetComponent<GPS>().unityCoor;
-        locationVec = checkloc;
+        locationVec = GPSEncoder.GPSToUCS(lat, lon);
     }
-    
-    // Update is called once per frame
-    void Update()
-    {
-        KeybordMove();
-    }
-    void location()
-    {
-        locationVec.y += 10; 
-        transform.position = locationVec;
-        
-    }
-
-    void KeybordMove()
-    {
-        Vector3 mov = transform.forward * Input.GetAxis("Vertical");
-        Vector3 rot = new Vector3(0, Input.GetAxis("Horizontal"), 0);
-        transform.position += (mov * mov_speed * Time.deltaTime);
-        transform.Rotate(rot * rot_speed * Time.deltaTime);
-
-
-    }
+    //void KeybordMove()
+    //{
+    //    //Vector3 mov = transform.forward * Input.GetAxis("Vertical");
+    //    //transform.position += (mov * mov_speed * Time.deltaTime);
+    //}
 }
