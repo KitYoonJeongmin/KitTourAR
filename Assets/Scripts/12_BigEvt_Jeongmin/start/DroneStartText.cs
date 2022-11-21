a7fad53ca9c1535ef06ce0786e75240b1c527e5c
KitYoonJeongmin <102850131+KitYoonJeongmin@users.noreply.github.com>
2022-11-21 14:01:49 +0900

    end Scene Script

--- Assets/Scripts/12_BigEvt_Jeongmin/start/DroneStartText.cs
+++ Assets/Scripts/12_BigEvt_Jeongmin/start/DroneStartText.cs
@@ -8,36 +8,63 @@ public class DroneStartText : MonoBehaviour
     public GameObject projectile;
     private GameObject drone;
     private Text textViewport;
-    private string[] textList = new string[4];
+    private string[] startTextList = new string[4];
+    private List<string> endTextList = new List<string>();
     private int textIndex;
+    private string sceneName;
     void Start()
     {
+        sceneName = SceneManager.GetActiveScene().name;
         drone = GameObject.Find("Drone");
         textIndex = 0;
-        textList[0] = "우와!! 드론이다!! \n 금오공대는 학생들이 드론도 날리는구나!!";
-        textList[1] = "근데 운동장에서 드론을 날려도 되나?\n 누가 조종하고 있는거지?";
-        textList[2] = "으악! 주인도 없는 드론이 공격하잖아!";
-        textList[3] = "<드론> \n\n 반갑다. 인간들아. \n인공지능의. 시대가. 도래했다.";
+
+        startTextList[0] = "우와!! 드론이다!! \n 금오공대는 학생들이 드론도 날리는구나!!";
+        startTextList[1] = "근데 운동장에서 드론을 날려도 되나?\n 누가 조종하고 있는거지?";
+        startTextList[2] = "으악! 주인도 없는 드론이 공격하잖아!";
+        startTextList[3] = "<드론> \n\n 반갑다. 인간들아. \n인공지능의. 시대가. 도래했다.";
+
+        endTextList.Add("헉... 꿈이었잖아... 언제 깜빡 잠들었지?");
+        endTextList.Add("오늘 학교에서 신기한 기기들을 많이봐서 그런가 별 꿈을 다꾸네..");
+        endTextList.Add("<정보>\n\n 금오공과 대학교의 드론은 공격기능이 없다.");
+        endTextList.Add("<정보>\n\n 보통 드론은 사람이 드문 본관 옆 잔디밭에서 날리니 안심하자.");
+
+
         textViewport = GameObject.Find("DialogText").GetComponent<Text>();
-        textViewport.text = textList[textIndex];
+        if (sceneName.Contains("Start")) { textViewport.text = startTextList[textIndex]; }
+        else if(sceneName.Contains("End")) { textViewport.text = startTextList[textIndex]; }
+        
     }
     public void Click()
     {
         textIndex++;
-        if (textIndex == 4)
+        if(sceneName.Contains("Start"))
         {
-            SceneManager.LoadScene("BigEvtDroneShooter");
-            return;
-        }
-        textViewport.text = textList[textIndex];
-        textViewport.text = textViewport.text.Replace("\\n", "\n");
+            if (textIndex == 4)
+            {
+                SceneManager.LoadScene("BigEvtDroneShooter");
+                return;
+            }
+            textViewport.text = startTextList[textIndex];
+            textViewport.text = textViewport.text.Replace("\\n", "\n");
 
-        if (textIndex == 2)
-        {
-            drone.transform.LookAt(GameObject.Find("AR Camera").transform.position);
-            GameObject shot = Instantiate(projectile, drone.transform.position, transform.rotation);
-            //shot.transform.position = Vector3.MoveTowards(shot.transform.position, GameObject.Find("Camera").transform.position, 5);
-            shot.transform.GetComponent<Rigidbody>().AddForce(drone.transform.forward * 1000.0f);
+            if (textIndex == 2)
+            {
+                drone.transform.LookAt(GameObject.Find("AR Camera").transform.position);
+                GameObject shot = Instantiate(projectile, drone.transform.position, transform.rotation);
+                //shot.transform.position = Vector3.MoveTowards(shot.transform.position, GameObject.Find("Camera").transform.position, 5);
+                shot.transform.GetComponent<Rigidbody>().AddForce(drone.transform.forward * 1000.0f);
+            }
         }
+        else if(sceneName.Contains("End"))
+        {
+            if (textIndex == 4)
+            {
+                SceneManager.LoadScene("KitMapScene");
+                return;
+            }
+            textViewport.text = startTextList[textIndex];
+            textViewport.text = textViewport.text.Replace("\\n", "\n");
+        }
+        
     }
 }
